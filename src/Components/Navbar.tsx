d14b84c073e961eba3defcfa5e662c7c37c8f2dc
Akash Gupta <akashgupta989322@gmail.com>
2023-11-20 20:08:09 +0530

    update

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,9 @@
+import style from '../CSS/navbar.module.scss'
+import logo from '../assets/Logo.png'
+export default function Navbar() {
+    return (
+        <div>
+            <img src={logo} alt="" />
+        </div>
+    )
+}
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,8 @@
+import style from '../CSS/home.module.scss'
+export default function Home() {
+  return (
+    <div>
+        <h1 className={style.head}>okok</h1>
+    </div>
+  )
+}
