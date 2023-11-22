d5bda1b2dfb44dc8437660087daa216c36c8146a
Akash Gupta <akashgupta989322@gmail.com>
2023-11-22 15:24:38 +0530

    update

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const connection = require("./db");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+app.listen(3000, async () => {
+  try {
+    await connection;
+    console.log(`Listening on port 3000`);
+  } catch (error) {
+    console.log(error);
+  }
+});
