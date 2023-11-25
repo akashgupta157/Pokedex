82bf44e9f8e649f46b43040949891d8622712faf
Akash Gupta <akashgupta989322@gmail.com>
2023-11-25 11:19:15 +0530

    update

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,42 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 import style from '../CSS/home.module.scss'
 import Loader from '../Components/Loader'
-import { useQuery, gql } from '@apollo/client';
 export default function Home() {
   const navigate = useNavigate()
-  const { loading, error, data } = useQuery(gql`
-  {
-    pokemons(first:200) {
-      id
-      name
-      number
-      image
-      types
-    }
-  }
-`);
-  if (loading) return <Loader />;
-  if (error) return <p>Error: {error.message}</p>;
   return (
-    <div style={{ backgroundColor: '#f0f0f0' }}>
-      <div className={style.grid}>
-        {data.pokemons.map((pokemon: any) => (
-          <section key={pokemon.id} onClick={() => navigate(`/${pokemon.name}/${pokemon.id}`)}>
-            <img src={pokemon.image} alt={pokemon.name} />
-            <p>#{pokemon.number}</p>
-            <h1>{pokemon.name}</h1>
-            <div>
-              {
-                pokemon.types.map((type: any, index: number) => (
-                  <p key={index} style={{
-                    backgroundColor: type === 'Grass' ? '#79c851' : type === 'Fire' ? "#f18031" : type === 'Poison' ? "#a040a0" : type === 'Flying' ? "#a990f1" : type === 'Water' ? "#6891f1" : type === 'Bug' ? "#a8b921" : type === 'Normal' ? "#a8a979" : type === 'Electric' ? "#f9d031" : type === 'Ground' ? "#e0c169" : type === 'Fairy' ? "#ee98ad" : type === 'Fighting' ? "#c03028" : type === 'Psychic' ? "#f95988" : type === 'Steel' ? "#b8b8d0" : type === 'Ice' ? "#99d8d9" : type === 'Rock' ? "#8c7e40" : type === 'Dark' ? "#705949" : type === 'Dragon' ? "#7139f8" : type === 'Ghost' ? "#705999" : '#68a090'
-                  }}>{type}</p>
-                ))
-              }
-            </div>
-          </section>
-        ))}
-      </div>
-    </div >
+    <></>
+    // <div style={{ backgroundColor: '#f0f0f0' }}>
+    //   <div className={style.grid}>
+    //     {data.pokemons.map((pokemon: any) => (
+    //       <section key={pokemon.id} onClick={() => navigate(`/${pokemon.name}/${pokemon.id}`)}>
+    //         <img src={pokemon.image} alt={pokemon.name} />
+    //         <p>#{pokemon.number}</p>
+    //         <h1>{pokemon.name}</h1>
+    //         <div>
+    //           {
+    //             pokemon.types.map((type: any, index: number) => (
+    //               <p key={index} style={{
+    //                 backgroundColor: type === 'Grass' ? '#79c851' : type === 'Fire' ? "#f18031" : type === 'Poison' ? "#a040a0" : type === 'Flying' ? "#a990f1" : type === 'Water' ? "#6891f1" : type === 'Bug' ? "#a8b921" : type === 'Normal' ? "#a8a979" : type === 'Electric' ? "#f9d031" : type === 'Ground' ? "#e0c169" : type === 'Fairy' ? "#ee98ad" : type === 'Fighting' ? "#c03028" : type === 'Psychic' ? "#f95988" : type === 'Steel' ? "#b8b8d0" : type === 'Ice' ? "#99d8d9" : type === 'Rock' ? "#8c7e40" : type === 'Dark' ? "#705949" : type === 'Dragon' ? "#7139f8" : type === 'Ghost' ? "#705999" : '#68a090'
+    //               }}>{type}</p>
+    //             ))
+    //           }
+    //         </div>
+    //       </section>
+    //     ))}
+    //   </div>
+    // </div >
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './CSS/index.scss'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-const client = new ApolloClient({
-  uri: 'https://graphql-pokemon2.vercel.app',
-  cache: new InMemoryCache(),
-});
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
